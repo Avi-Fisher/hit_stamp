15cdb32569e4efdf3ed733d1e4b62c1f4229a968
Avi Fisher <avifisher82@gmail.com>
2025-10-29 09:49:13 +0200

    creat 'ganerate_secret' function

--- game/secret.py
+++ game/secret.py
@@ -1,3 +1,17 @@
-def generate_secret():
-    return ""
+import random
+from random import  randint
+
+def generate_secret(length):
+    secret = ""
+    count = 0
+
+    while count < 4:
+        number = randint(0, 9)
+
+        if str(number) not in secret:
+            secret += str(number)
+            secret += " "
+            count += 1
+
+    return secret
 
