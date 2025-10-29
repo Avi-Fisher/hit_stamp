1ff705b6a80e61a8bbaa32460774460cb57d200e
Avi Fisher <avifisher82@gmail.com>
2025-10-29 11:03:00 +0200

    creat 'score_guess' function

--- game/logic.py
+++ game/logic.py
@@ -1,4 +1,17 @@
-def score_guess():
+def score_guess(secret,guess):
+    bulls = 0
+    cows = 0
+
+    for i in range(4):
+        if secret[i] == guess[i]:
+            bulls += 1
+        elif guess[i] in secret:
+            cows += 1
+
+    return (bulls,cows)
+
+
+
     return "tuple"
 
 def is_won():
